0af12d97b3388edd68d93b8e414fcacc339b9651
herik-erbeth <herik.docarmo@hotmail.com>
2025-03-20 16:20:08 -0300

    test(repository): convert integration test to unit test using mocks
    Refactor tests to mock repository methods instead of using a real database.
    - Replaced @DataNeo4jTest with @SpringBootTest and @MockitoBean for mocking the repository.
    - Updated tests to mock findByName and findByTeammatesName methods.

--- src/test/java/demo/repository/PersonRepositoryTest.java
+++ src/test/java/demo/repository/PersonRepositoryTest.java
@@ -2,63 +2,51 @@ package demo.repository;
 
 import demo.TestData;
 import demo.model.Person;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
-@DataNeo4jTest
+@SpringBootTest
 public class PersonRepositoryTest {
 
-    @Autowired
+    @MockitoBean
     private PersonRepository repository;
 
-    @BeforeEach
-    void setup() {
-        repository.deleteAll();
+    @Test
+    void shouldFindByName() {
+        Person person = TestData.person();
+        when(repository.findByName("Greg")).thenReturn(person);
 
+        Person personResult = repository.findByName("Greg");
+
+        assertThat(personResult).isNotNull();
+        assertThat(personResult.getName()).isEqualTo(person.getName());
+        verify(repository, times(1)).findByName("Greg");
+    }
+
+    @Test
+    void shouldFindByTeammatesName() {
         Person person = TestData.person();
         Person person1 = TestData.person1();
         Person person2 = TestData.person2();
-
-        repository.save(person);
-        repository.save(person1);
-        repository.save(person2);
 
         person.worksWith(person1);
         person.worksWith(person2);
         person1.worksWith(person2);
 
-        repository.save(person);
-        repository.save(person1);
-    }
+        when(repository.findByTeammatesName("Greg")).thenReturn(List.of(person1, person2));
 
-    @Test
-    void shouldFindByName() {
-        Person person = repository.findByName(TestData.person().getName());
-
-        assertThat(person).isNotNull();
-        assertThat(person.getName()).isEqualTo(TestData.person().getName());
-    }
-
-    @Test
-    void shouldFindByTeammatesName() {
-        List<Person> teammates = repository.findByTeammatesName(TestData.person().getName());
+        List<Person> teammates = repository.findByTeammatesName("Greg");
 
         assertThat(teammates).isNotEmpty();
         assertThat(teammates).hasSize(2);
         assertThat(teammates).extracting(Person::getName).containsExactlyInAnyOrder(
                 TestData.person1().getName(),
                 TestData.person2().getName());
-    }
-
-    @AfterAll
-    static void cleanUp(@Autowired PersonRepository repository) {
-        repository.deleteAll();
     }
 }
